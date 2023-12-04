1b3636ee711352b83ad3d90ef0d0870fcbcb8630
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-12-04 09:47:50 +0200

    json to csv updated

--- jsonToCsv.js
+++ jsonToCsv.js
@@ -1,15 +1,62 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const input = fs.readFileSync("./input.json", 'utf-8');
+const input = fs.readFileSync("./input.json", "utf-8");
 const jsonData = JSON.parse(input);
-const headers = Object.keys(jsonData[0]);
-const csvHeaders = headers.join(',');
 
-const csvContent = jsonData.map(row => headers.map(header => row[header])).join(',');
+function jsonToCsv(jsonData) {
+  class Product {
+    constructor(productId, priceEur, priceRon, isActive) {
+      this.productId = productId;
+      this.priceEur = priceEur;
+      this.priceRon = priceRon;
+      this.isActive = isActive;
+    }
+  }
+  const products = [];
+  jsonData.records.forEach((element) => {
+    const productId = element.Product2.Article_SAP_Code__c;
+    const isActive = element.IsActive;
+    let priceEur = null;
+    let priceRon = null;
+    const items = jsonData.records.filter(
+      (x) => x.Product2.Article_SAP_Code__c === productId
+    );
 
-const result  =[csvHeaders,csvContent].join('\n');
-fs.writeFileSync('output.csv', result, 'utf8');
+    items.forEach((item) => {
+      if (item.CurrencyIsoCode === "EUR") {
+        priceEur = item.UnitPrice;
+      } else if (item.CurrencyIsoCode === "RON") {
+        priceRon = item.UnitPrice;
+      }
+    });
 
+    const hasProduct = products.some((p) => p.productId === productId);
+    if (!hasProduct) {
+      products.push(new Product(productId, priceEur, priceRon, isActive));
+    }
+  });
 
+  const headers = Object.keys(products[0]);
+  const csvHeader =[
+      "Product StockKeepingUnit",
+      "Price (01s68000002d6IvAAI) EUR",
+      "Price (01s68000002d6IvAAI) RON",
+      "IsActive",
+    ].join(',') + "\n";
 
+  const csvRows = products
+    .map((product) => {
+      return headers
+        .map((header) => {
+          return product[header];
+        })
+        .join(",");
+    })
+    .join("\n");
+
+  return csvHeader + csvRows;
+}
+
+const csvContent = jsonToCsv(jsonData);
+fs.writeFileSync("output.csv", csvContent, "utf8");
 
