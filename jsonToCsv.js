324ec7226642ed9f125f0a3ceaf6eadce0108690
VelislavaC4Nexus <velislava.marinova.c4Nexus@gmail.com>
2023-12-05 22:05:55 +0200

    use fs promises for async read and write files, code optimization

--- jsonToCsv.js
+++ jsonToCsv.js
@@ -1,62 +1,68 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-const input = fs.readFileSync("./input.json", "utf-8");
-const jsonData = JSON.parse(input);
+async function processJsonToCsv() {
+  try {
+    const input = await fs.readFile("./input.json", "utf-8");
+    const jsonData = JSON.parse(input);
 
-function jsonToCsv(jsonData) {
-  class Product {
-    constructor(productId, priceEur, priceRon, isActive) {
-      this.productId = productId;
-      this.priceEur = priceEur;
-      this.priceRon = priceRon;
-      this.isActive = isActive;
-    }
-  }
-  const products = [];
-  jsonData.records.forEach((element) => {
-    const productId = element.Product2.Article_SAP_Code__c;
-    const isActive = element.IsActive;
-    let priceEur = null;
-    let priceRon = null;
-    const items = jsonData.records.filter(
-      (x) => x.Product2.Article_SAP_Code__c === productId
-    );
-
-    items.forEach((item) => {
-      if (item.CurrencyIsoCode === "EUR") {
-        priceEur = item.UnitPrice;
-      } else if (item.CurrencyIsoCode === "RON") {
-        priceRon = item.UnitPrice;
+    function jsonToCsv(jsonData) {
+      class Product {
+        constructor(productId, priceEur, priceRon, isActive) {
+          this.productId = productId;
+          this.priceEur = priceEur;
+          this.priceRon = priceRon;
+          this.isActive = isActive;
+        }
       }
-    });
+      const products = [];
+      jsonData.records.forEach((element) => {
+        const productId = element.Product2.Article_SAP_Code__c;
+        const isActive = element.IsActive;
+        const currencyIsoCode = element.CurrencyIsoCode;
+        const unitPrice = element.UnitPrice;
 
-    const hasProduct = products.some((p) => p.productId === productId);
-    if (!hasProduct) {
-      products.push(new Product(productId, priceEur, priceRon, isActive));
-    }
-  });
+        const hasProduct = products.some(p => p.productId === productId);
+        if (!hasProduct) {
+          products.push(new Product(productId, null, null, isActive));
+        }
 
-  const headers = Object.keys(products[0]);
-  const csvHeader =[
-      "Product StockKeepingUnit",
-      "Price (01s68000002d6IvAAI) EUR",
-      "Price (01s68000002d6IvAAI) RON",
-      "IsActive",
-    ].join(',') + "\n";
+        const existingProduct = products.find((p) => p.productId === productId);
+        if (currencyIsoCode === "EUR" && existingProduct.priceEur === null) {
+          existingProduct.priceEur = unitPrice;
+        } else if (
+          currencyIsoCode === "RON" &&
+          existingProduct.priceRon === null
+        ) {
+          existingProduct.priceRon = unitPrice;
+        }
+      });
+      const headers = Object.keys(products[0]);
+      const csvHeader =[
+          "Product StockKeepingUnit",
+          "Price (01s68000002d6IvAAI) EUR",
+          "Price (01s68000002d6IvAAI) RON",
+          "IsActive",
+        ].join(",") + "\n";
 
-  const csvRows = products
-    .map((product) => {
-      return headers
-        .map((header) => {
-          return product[header];
+      const csvRows = products
+        .map((product) => {
+          return headers
+            .map((header) => {
+              return product[header];
+            })
+            .join(",");
         })
-        .join(",");
-    })
-    .join("\n");
+        .join("\n");
 
-  return csvHeader + csvRows;
+      return csvHeader + csvRows;
+    }
+
+    const csvContent = jsonToCsv(jsonData);
+    await fs.writeFile("output1.csv", csvContent, "utf8");
+  } catch (error) {
+    throw new Error('An error occurred while processing JSON to CSV', error);
+  }
 }
 
-const csvContent = jsonToCsv(jsonData);
-fs.writeFileSync("output13.csv", csvContent, "utf8");
+processJsonToCsv();
 
